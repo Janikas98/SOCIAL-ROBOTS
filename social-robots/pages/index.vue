06e09b1d6226b4c5c8af476964502189bcfb7775
JoaoGuerreiro <joao.guerreiro@eviso.it>
2022-01-30 01:40:43 +0000

    config v2

---
+++ social-robots/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center" align="center">
+      <v-col cols="12">
+        <v-card>
+          <v-card-text>
+            NAO VIDEO
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="12">
+        <v-card>
+          <v-card-title class="headline" style="text-align: center !important; display: block;">
+            Built with advanced AI.
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center" class="my-6 mx-0">
+      <v-btn
+        elevation="3"
+        x-large
+        style="width: 100%"
+        color="success"
+      >START</v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
