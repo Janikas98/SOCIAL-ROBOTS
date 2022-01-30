7383149caaaa3fd347fe6d607a38af471393314d
JoaoGuerreiro <joao.guerreiro@eviso.it>
2022-01-30 01:26:48 +0000

    config

---
+++ social-robots/layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container fluid>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      app
+    >
+      <span>RSIHR Course &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+html, body {
+  overflow: hidden;
+}
+</style>
