7c2c2ca14881d0e11a7aecad719c3cd7f740da74
miquelroq <miguelroque99@tecnico.ulisboa.pt>
2022-02-09 11:40:30 +0000

    consent

--- social-robots/pages/nao-introduction.vue
+++ social-robots/pages/nao-introduction.vue
@@ -24,18 +24,18 @@
           <v-card-title class="headline" style="text-align: left !important; display: block; font-size: 25px !important; padding: 5px;">
             <div v-if="$route.query.ai != 'no_robot'">
               <span style="width: 50%;">
-                I'm  going to help you play a simplified version of Wordle! 
+                I'm  going to help you play a simplified version of Wordle!
               </span><br />
               <span style="width: 50%;">
                  You will have to guess a five letter word in five attempts. At each attempt, you will be able to choose between 2 words.
               </span>
               <br />
               <span style="width: 50%;">
-                I will point towards the word I recommend, you can follow my recommendation or not. 
+                I will point towards the word I recommend, you can follow my recommendation or not.
               </span>
               <br />
               <span style="width: 50%;">
-                 After each guess, the color of the letters will change to show how close your guess was to the word. 
+                 After each guess, the color of the letters will change to show how close your guess was to the word.
               </span>
               <br />
               <span style="width: 50%; color: yellow;" >
@@ -46,21 +46,21 @@
               <span style="width: 50%; color: green;" >
               Green</span>
               <span style="width: 50%;">
-                if it's in the correct position and 
+                if it's in the correct position and
               </span>
               <span style="width: 50%; color: red;" >
               Red</span>
               <span style="width: 50%;">
-                if the letter is not in the word! 
-              </span> 
-              <br /> 
+                if the letter is not in the word!
+              </span>
+              <br />
               <span style="width: 50%;">
-                Have fun and good luck! 
-              </span>  
+                Have fun and good luck!
+              </span>
             </div>
             <div v-else-if="$route.query.ai == 'no_robot'">
               <span style="width: 50%;">
-                I'm  going to help you play a simplified version of Wordle! 
+                I'm  going to help you play a simplified version of Wordle!
               </span><br />
               <span style="width: 50%;">
                  You will have to guess a five letter word in five attempts. At each attempt, you will be able to choose between 2 words.
@@ -72,11 +72,11 @@
               <span style="width: 50%; color: #FFBF00; font-weight:bold;" >
               yellow</span>
               <span style="width: 50%;">
-                the word I recommend, you can follow my recommendation or not. 
+                the word I recommend, you can follow my recommendation or not.
               </span>
               <br />
               <span style="width: 50%;">
-                 After each guess, the color of the letters will change to show how close your guess was to the word. 
+                 After each guess, the color of the letters will change to show how close your guess was to the word.
               </span>
               <br />
               <span style="width: 50%; color: yellow;" >
@@ -87,17 +87,17 @@
               <span style="width: 50%; color: green;" >
               Green</span>
               <span style="width: 50%;">
-                if it's in the correct position and 
+                if it's in the correct position and
               </span>
               <span style="width: 50%; color: red;" >
               Red</span>
               <span style="width: 50%;">
-                if the letter is not in the word! 
-              </span> 
-              <br /> 
+                if the letter is not in the word!
+              </span>
+              <br />
               <span style="width: 50%;">
-                Have fun and good luck! 
-              </span>  
+                Have fun and good luck!
+              </span>
             </div>
           </v-card-title>
         </v-card>
@@ -107,37 +107,37 @@
       <v-col cols="12">
         <v-card style="height: fit-content; padding: 15px;">
           <v-card-title class="headline" style="text-align: left !important; display: block; font-size: 25px !important; padding: 5px;">
-            <div v-if="$route.query.ai != 'no_robot'">
+            <div>
               <span style="width: 50%;">
                 You may withdraw from this project at any time. All the information collected will be made anonymous.
               </span>
               <br />
               <span style="width: 50%;">
                   During the course of the task,  your screen will be observed by a member of our research team who will annotate your responses.
-              </span> 
+              </span>
               <br />
               <span style="width: 50%;">
                   After the completion of the task, you will be asked to fill out a questionnaire to rate your experience.
-              </span> 
+              </span>
               <br />
               <span style="width: 50%;">
                   Your responses will be made anonymous and will provide data for research and educational purposes only.
-              </span> 
+              </span>
               <br />
               <span style="width: 50%;">
                   You are required to be over the age of 18 for participating in this study.
-              </span> 
+              </span>
               <br />
               <span style="width: 50%;">
                   If you have any concerns about the treatment of your rights as a research subject, you may contact our team at rsihr.project@gmail.com.
-              </span> 
+              </span>
               <br />
               <span style="width: 50%;">
                   POTENTIAL RISKS: none
-              </span> 
+              </span>
               <br />
               <span style="width: 50%;">
-                  By clicking the START button you are hereby consenting to participating in this study, and acknowledging you have reviewed this consent form.   
+                  By clicking the START button you are hereby consenting to participating in this study, and acknowledging you have reviewed this consent form.
               </span>
             </div>
           </v-card-title>
